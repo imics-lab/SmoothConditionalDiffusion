725c513f822ed293138aac5b00162c871100f605
Gentry Atkinson <gma23@txstate.edu>
2023-02-03 16:03:01 -0600

    getting non-diffusion data maker running

--- datasets.py
+++ datasets.py
@@ -6,8 +6,27 @@
 
 import torch
 import os
+from gen_ts_data import generate_signal_as_tensor
 
 DATA_DIRECTORY = 'data'
+
+def get_noisy_synthetic_dataset(args):
+    SIGNAL_LENGTH = 128
+    SET_LENGTH = 5001
+    NUM_CHANNELS = 1
+    sig_dict = {
+        'avg_pattern_length' : [torch.randint(1, 10) for _ in args.num_classes],
+        'avg_amplitude' : [torch.randint(1, 10) for _ in args.num_classes],
+        'default_variance' : [torch.randint(1, 5) for _ in args.num_classes],
+        'variance_pattern_length' : [torch.randint(1, 20) for _ in args.num_classes],
+        'variance_amplitude' : [torch.randint(1, 5) for _ in args.num_classes],
+    }
+    y_clean = torch.randint(SET_LENGTH)
+    y_noisy = torch.zeros(SET_LENGTH)
+    X = torch.empty(SET_LENGTH, NUM_CHANNELS, SIGNAL_LENGTH)
+    for i in range(SET_LENGTH):
+        pass
+
 
 def load_dataset(args):
     if not os.path.exists(DATA_DIRECTORY):
@@ -17,3 +36,4 @@ def load_dataset(args):
             print("Synthetic 5 dataset located")
         else:
             print("Generating Synthetic 5 dataset")
+            X, y_clean, y_noisy = get_noisy_synthetic_dataset(args)
